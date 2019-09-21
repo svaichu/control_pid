8a3c901808fa87203a63fb2b2807830885248cd8
VaishnavaHari S <cb.en.u4mee16159@cb.students.amrita.edu>
2019-09-21 11:02:20 +0530

    Update crawler.m

--- crawler.m
+++ crawler.m
@@ -1,4 +1,9 @@
-fileID = fopen('working.txt','r');
+%remove all empty line in text file and format it like P.txt
+
+fileID = fopen('P.txt','r');    %change the name of the file
+
+fgetl(fileID);
 formatSpec = '           %f           %f           %f           %f           %f';
 sizeA = [5 Inf];
 A = fscanf(fileID,formatSpec,sizeA);
+plot(A(1,:),A(2:5,:))   % u can add legend and label axis
